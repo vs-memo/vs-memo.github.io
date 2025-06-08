ac6863f5138a702f8054581d2243ed0e907bfc55
vs-memo <215248321+vs-memo@users.noreply.github.com>
2025-06-08 18:37:55 +0900

    Quartz sync: Jun 8, 2025, 6:37 PM

--- deploy-openurl.bat
+++ deploy-openurl.bat
@@ -14,12 +14,12 @@ REM 管理者権限でGit Bashを実行し、コマンドを実行、その後�
 powershell -Command "Start-Process '%GIT_BASH_PATH%' -Verb RunAs -ArgumentList '-c \"%GIT_COMMAND%; exit\"'"
 
 REM 3秒間待機する
-echo 3秒間待機します...
-timeout /t 3 /nobreak
+REM echo 3秒間待機します...
+REM timeout /t 3 /nobreak
 
 REM 指定したWebページを開く
-echo Webページを開きます: %WEB_URL%
-start "" "%WEB_URL%"
+REM echo Webページを開きます: %WEB_URL%
+REM start "" "%WEB_URL%"
 
 REM 元のディレクトリに戻る (オプション)
 popd
